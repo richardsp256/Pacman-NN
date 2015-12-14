e445c208ddd7142cc186beb570a3709f67c1d41f
Ezra Dowd <ezra.dowd@gmail.com>
2015-12-14 16:45:59 -0500

    updated params

--- params.js
+++ params.js
@@ -1,10 +1,10 @@
 Params.PACMANCOUNT = 50;
-Params.BIAS = .1;
+Params.BIAS = -1;
 Params.POPULATION = 50;
-Params.MUTATION_RATE = 0.25;
-Params.CROSS_RATE = 0.2;
-Params.WEIGHT_COUNT = 10;
-Params.PERTURBATION_RATE = 0.25;
+Params.MUTATION_RATE = 0.1;
+Params.CROSS_RATE = 0.7;
+Params.WEIGHT_COUNT = 14;
+Params.PERTURBATION_RATE = 0.3;
 Params.THRESHOLD = 0.5;
 Params.ELITE_COUNT = 4;         //Number of "elite" genomes to sample from.
-Params.ELITE_NUMBER = 4;        //Index of the "elite" ?
+Params.ELITE_NUMBER = 1;        //Index of the "elite" ?
